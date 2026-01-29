2b52d4d6167c49bf64f4d84851bdc8eaa0d0ffae
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-01-30 01:55:27 +0530

    feat: add EventService interface and implement EventService logic

---
+++ src/main/java/edu/icet/service/EventService.java
@@ -0,0 +1,19 @@
+package edu.icet.service;
+
+import edu.icet.dto.EventDto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface EventService {
+    void addEvent(EventDto event);
+    void updateEvent(EventDto event);
+    void deleteEvent(Integer eventId);
+    List<EventDto>getAllEventDetail();
+    EventDto searchById(Integer eventId);
+    List<EventDto>searByuserId(Integer userId);
+    List<EventDto>searBytype(String type);
+    List<EventDto>searBylocation(String location);
+    List<EventDto>searBydate(LocalDate date);
+    List<EventDto>searBybudget(Double budget);
+}
