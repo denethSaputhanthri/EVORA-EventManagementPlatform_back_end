c08663520f158cc068913e78971762bcdd4777c6
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-01-30 02:00:30 +0530

    feat:create service layer add

---
+++ src/main/java/edu/icet/service/impl/EventServiceImpl.java
@@ -0,0 +1,50 @@
+package edu.icet.service.impl;
+
+import edu.icet.dto.UserDto;
+import edu.icet.role.Role;
+import edu.icet.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class EventServiceImpl implements UserService {
+    @Override
+    public void addUser(UserDto user) {
+
+    }
+
+    @Override
+    public void updateUser(UserDto user) {
+
+    }
+
+    @Override
+    public void deleteUser(Integer userId) {
+
+    }
+
+    @Override
+    public List<UserDto> getUserDetails() {
+        return List.of();
+    }
+
+    @Override
+    public UserDto searchById(Integer userId) {
+        return null;
+    }
+
+    @Override
+    public List<UserDto> searchByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public List<UserDto> searchByEmail(String email) {
+        return List.of();
+    }
+
+    @Override
+    public List<UserDto> searchByRole(Role role) {
+        return List.of();
+    }
+}
