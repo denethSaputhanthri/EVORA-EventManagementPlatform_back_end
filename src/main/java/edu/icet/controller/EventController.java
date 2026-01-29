05a0529bb2371f217c6d49119cdfeac11e1fc63d
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-01-30 02:08:30 +0530

    feat: EventService,EventRepository and Model mapper constructor inject

--- src/main/java/edu/icet/controller/EventController.java
+++ src/main/java/edu/icet/controller/EventController.java
@@ -1,11 +1,17 @@
 package edu.icet.controller;
 
+import edu.icet.service.EventService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/Event")
 @Tag(name = "Event-Controller",description = "Event API Collection...")
 public class EventController {
+
+    final EventService service;
+
 }
--- src/main/java/edu/icet/service/impl/EventServiceImpl.java
+++ src/main/java/edu/icet/service/impl/EventServiceImpl.java
@@ -1,13 +1,21 @@
 package edu.icet.service.impl;
 
 import edu.icet.dto.UserDto;
+import edu.icet.repository.EventRepository;
 import edu.icet.role.Role;
 import edu.icet.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
+@RequiredArgsConstructor
 public class EventServiceImpl implements UserService {
+
+    final EventRepository repository;
+    final ModelMapper mapper;
+
     @Override
     public void addUser(UserDto user) {
 
