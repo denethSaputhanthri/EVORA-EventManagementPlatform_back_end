11786f558262f394230cbb7151d32a70c4017dd5
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-01-29 13:54:26 +0530

    feat: add User entity and DTO

---
+++ src/main/java/edu/icet/dto/UserDto.java
@@ -0,0 +1,17 @@
+package edu.icet.dto;
+
+import edu.icet.role.Role;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
+public class UserDto {
+    private Integer userId;
+    private String name;
+    private String email;
+    private String passwordHash;
+    private Role role;
+    private Date createAt;
+}
---
+++ src/main/java/edu/icet/entity/UserEntity.java
@@ -0,0 +1,26 @@
+package edu.icet.entity;
+
+import edu.icet.role.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "user")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+    private String name;
+    private String email;
+    private String passwordHash;
+    private Role role;
+    private Date createAt;
+}
