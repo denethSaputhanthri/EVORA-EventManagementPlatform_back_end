777ee4b9031507a48becc8f093b93dfe5da6fbcd
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-02-25 20:26:10 +0530

    feat : implement Category Function.

--- src/main/java/edu/icet/service/impl/CategoryServiceImpl.java
+++ src/main/java/edu/icet/service/impl/CategoryServiceImpl.java
@@ -1,6 +1,7 @@
 package edu.icet.service.impl;
 
 import edu.icet.model.Category;
+import edu.icet.repository.CategoryRepository;
 import edu.icet.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,33 +12,35 @@ import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
+    final CategoryRepository categoryRepository;
+
     @Override
     public void addCategory(Category category) {
-
+        categoryRepository.addCategory(category);
     }
 
     @Override
     public void updateCategory(Category category) {
-
+        categoryRepository.updateCategory(category);
     }
 
     @Override
     public void deleteCategory(Integer id) {
-
+        categoryRepository.deleteCategory(id);
     }
 
     @Override
     public List<Category> getAllCategory() {
-        return List.of();
+        return categoryRepository.getAllCategory();
     }
 
     @Override
     public Category searchCategoryByName(String name) {
-        return null;
+        return categoryRepository.searchCategoryByName(name);
     }
 
     @Override
     public Category searchCategoryById(Integer id) {
-        return null;
+        return categoryRepository.searchCategoryById(id);
     }
 }
