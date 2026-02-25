0ca370f69ebebbbfce813e4a1f52e233fae15684
ShanukaUpendraOfficial <shanukaupendra@gmail.com>
2026-02-25 20:27:01 +0530

    feat : implement Event & Message Function.

--- src/main/java/edu/icet/repository/MessageRepository.java
+++ src/main/java/edu/icet/repository/MessageRepository.java
@@ -1,6 +1,19 @@
 package edu.icet.repository;
 
 
+import edu.icet.model.Message;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface MessageRepository {
+    void addMessage(Message message);
+    void updateMessage(Message message);
+    void deleteMessage(Integer id);
+    List<Message> getAllMessage();
+    Message searchMessageById(Integer id);
+    List<Message> searchMessageBySender(Integer senderId);
+    List<Message> searchMessageByReceiver(Integer receiverId);
+    List<Message> searchMessageByDate(LocalDate date);
 
 }
