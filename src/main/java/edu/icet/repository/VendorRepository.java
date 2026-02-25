2a3e21dae9b56cc58979520f80c12777546c3b8a
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-25 16:42:42 +0530

    feat: vendor repository,service db  native executions updated

--- src/main/java/edu/icet/repository/VendorRepository.java
+++ src/main/java/edu/icet/repository/VendorRepository.java
@@ -1,6 +1,17 @@
 package edu.icet.repository;
 
 
-public interface VendorRepository {
+import edu.icet.model.Vendor;
 
+import java.util.List;
+
+public interface VendorRepository {
+    boolean addVendor(Vendor vendor);
+    boolean updateVendor(Vendor vendor);
+    boolean deleteVendor(Integer id);
+    List<Vendor> getAllVendor();
+    Vendor searchVendorById(Integer id);
+    List<Vendor> searchVendorByUser(Integer userId);
+    Vendor searchVendorByName(String name);
+    Vendor searchVendorByPhone(String phone);
 }
--- src/main/java/edu/icet/service/impl/VendorServiceImpl.java
+++ src/main/java/edu/icet/service/impl/VendorServiceImpl.java
@@ -1,53 +1,55 @@
 package edu.icet.service.impl;
 
 import edu.icet.model.Vendor;
+import edu.icet.repository.VendorRepository;
 import edu.icet.service.VendorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 @RequiredArgsConstructor
 @Service
 public class VendorServiceImpl implements VendorService {
 
+    final private VendorRepository vendorRepository;
+
     @Override
     public void addVendor(Vendor vendor) {
-
+        vendorRepository.addVendor(vendor);
     }
 
     @Override
     public void updateVendor(Vendor vendor) {
-
+        vendorRepository.updateVendor(vendor);
     }
 
     @Override
     public void deleteVendor(Integer id) {
-
+        vendorRepository.deleteVendor(id);
     }
 
     @Override
     public List<Vendor> getAllVendor() {
-        return List.of();
+        return vendorRepository.getAllVendor();
     }
 
     @Override
     public Vendor searchVendorById(Integer id) {
-        return null;
+        return vendorRepository.searchVendorById(id);
     }
 
     @Override
     public List<Vendor> searchVendorByUser(Integer userId) {
-        return List.of();
+        return vendorRepository.searchVendorByUser(userId);
     }
 
     @Override
     public Vendor searchVendorByName(String name) {
-        return null;
+        return vendorRepository.searchVendorByName(name);
     }
 
     @Override
     public Vendor searchVendorByPhone(String phone) {
-        return null;
+        return vendorRepository.searchVendorByPhone(phone);
     }
 }
