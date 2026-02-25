fab1d24c99dbe45d6e94bc38e7af4ff194431cf5
Deneth Nadeeja <denethsaputhanthri@gmail.com>
2026-02-25 15:26:58 +0530

    feat: Wishlist repository,service db  native executions updated

--- src/main/java/edu/icet/repository/WishlistRepository.java
+++ src/main/java/edu/icet/repository/WishlistRepository.java
@@ -1,6 +1,18 @@
 package edu.icet.repository;
 
 
-public interface WishlistRepository {
+import edu.icet.model.Wishlist;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface WishlistRepository {
+    boolean addWishlist(Wishlist wishlist);
+    boolean updateWishlist(Wishlist wishlist);
+    boolean deleteWishlist(Integer id);
+    Wishlist searchWishlistById(Integer id);
+    List<Wishlist> getAllWishlist();
+    List<Wishlist> searchWishlistByUser(Integer userId);
+    List<Wishlist> searchWishlistByService(Integer serviceId);
+    List<Wishlist> searchWishlistByTime(LocalDateTime time);
 }
